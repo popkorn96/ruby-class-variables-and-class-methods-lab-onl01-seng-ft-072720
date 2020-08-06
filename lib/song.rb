ad9f59d725c0f0620302f2606b5d26b0c44b5189
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-06 17:14:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,8 @@ def self.artists
   @@artists.uniq
 end
 def self.genres 
-  @@genres 
+  @@genres
+  @@genres.uniq
 end
 def self.genre_count
   genre_count = {}
@@ -33,5 +34,19 @@ def self.genre_count
   end
   genre_count
 end
+
+
+
+
+def self.artist_count
+  artist_count = {}
+  @@artists. each do |artist|
+    if artist_count[artist]
+      artist_count[artist] += 1 
+    else
+      artist_count[artist] = 1 
+    end
+  end 
+  artist_count
 
 end

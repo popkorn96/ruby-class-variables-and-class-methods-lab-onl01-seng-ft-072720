ea7cad0d2181b1473135b7acf5c105d9fe3e314c
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-06 16:40:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,4 @@ class Song
   def initialize(name, artist, genre)
     @@song_count =+ 1 
   end
-  def self.name
-    puts "hit me baby one more time"
-  end
-  def self.artists
-    puts "Brittany Spears"
-  end
 end

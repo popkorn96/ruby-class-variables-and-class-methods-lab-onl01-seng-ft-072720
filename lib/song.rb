7b5d47188fae6b56f4c725a09b8729f5a8357576
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-06 16:47:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
 attr_accessor :name, :artist, :genre
 @@artists = []
 @@genres = []
-@@song_count = 0 
+@@count = 0 
 
 def initialize (name, artist, genre)
   @name = name
@@ -13,6 +13,6 @@ def initialize (name, artist, genre)
   @@song_count =+ 1 
 end
 def self.count 
-  @@song_count
+  @@count
 end
 end
